1eca337304ae934b79c26e7834eeacdbb32ea251
Junkyedh <22520393@gm.uit.edu.vn>
2025-03-26 02:23:59 +0700

    array

--- learnbasic/react-native/App.tsx
+++ learnbasic/react-native/App.tsx
@@ -1,30 +1,35 @@
 import { useState } from 'react';
-import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import { Button, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   
-  const [name, setName] = useState<string>("");
-  const [age, setAge] = useState<number>(0);
+const [students, setStudents] = useState([
+    { id: 1, name: 'John Doe', age: 20 },
+    { id: 2, name: 'Jane Doe', age: 21 },
+    { id: 3, name: 'John Smith', age: 22 },
+    { id: 4, name: 'Jane Smith', age: 23 },
+    { id: 5, name: 'John Brown', age: 24 },
+    { id: 6, name: 'Jane Brown', age: 25 },
+    { id: 7, name: 'John White', age: 26 },
+    { id: 8, name: 'Jane White', age: 27 },
+    { id: 9, name: 'John Black', age: 28 },
+    { id: 10, name: 'Jane Black', age: 29 },
+    { id: 11, name: 'John Green', age: 30 },
+    { id: 12, name: 'Jane Green', age: 31 },
+    { id: 13, name: 'John Blue', age: 32 },
+  ]);
   return (
     <View style={styles.container}>
-      <View>
-        <Text style={{fontSize: 60, fontWeight: '400'}}>Name: {name}</Text>
-        <TextInput 
-          multiline
-          style={{height: 60, borderColor: 'gray', borderWidth: 1, padding: 10, width: 200}}
-          onChangeText={(value) => setName(value)}
-        />
-      </View>
-
-      <View>
-        <Text style={{fontSize: 60, fontWeight: '400'}}>Age: {age}</Text>
-        <TextInput
-          keyboardType='numeric'
-          maxLength={2}
-          style={{height: 60, borderColor: 'gray', borderWidth: 1, padding: 10, width: 200}}
-          onChangeText={(value) => setAge(+value)}
-        />
-      </View>
+      <Text style={{alignSelf: "center", paddingVertical: 20, fontSize: 30}}>List Students</Text>
+      <ScrollView>
+        {students.map(students => {
+          return (
+            <View key = {students.id} style={{padding: 30, backgroundColor: 'lightblue', marginBottom: 30}}>
+              <Text>{students.id}. {students.name} - {students.age}</Text>
+            </View>
+          );
+        })}
+      </ScrollView> 
     </View>
   );
 }
@@ -34,7 +39,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 50,
+    paddingHorizontal: 20,
+
   },
 });
